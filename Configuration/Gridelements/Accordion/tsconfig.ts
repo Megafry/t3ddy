eb4d9533a16bb4c64bec4c099b06cc75a3e0d184
Armin Vieweg <armin@v.ieweg.de>
2016-02-14 21:43:54 +0100

    [FEATURE] Add new option to accordion container: Singe Page Mode
    When checked, in accordions only one page can be opened at once.

--- Configuration/Gridelements/Accordion/tsconfig.ts
+++ Configuration/Gridelements/Accordion/tsconfig.ts
@@ -20,4 +20,35 @@ tx_gridelements.setup.t3ddy-accordion {
 			}
 		}
 	}
+	flexformDS (
+		<T3DataStructure>
+			<meta>
+				<langDisable>1</langDisable>
+			</meta>
+			<ROOT type="array">
+				<type>array</type>
+				<el type="array">
+					<singlePageMode type="array">
+						<TCEforms type="array">
+							<label>LLL:EXT:t3ddy/Configuration/Gridelements/Accordion/locallang.xml:option.singlePageMode</label>
+							<config type="array">
+								<type>select</type>
+								<items type="array">
+									<numIndex index="0" type="array">
+										<numIndex index="0">LLL:EXT:t3ddy/Configuration/Gridelements/Accordion/locallang.xml:option.off</numIndex>
+										<numIndex index="1"></numIndex>
+									</numIndex>
+									<numIndex index="1" type="array">
+										<numIndex index="0">LLL:EXT:t3ddy/Configuration/Gridelements/Accordion/locallang.xml:option.on</numIndex>
+										<numIndex index="1">single-page-mode</numIndex>
+									</numIndex>
+								</items>
+								<default>single-page-mode</default>
+								</config>
+						</TCEforms>
+					</singlePageMode>
+				</el>
+			</ROOT>
+		</T3DataStructure>
+	)
 }
