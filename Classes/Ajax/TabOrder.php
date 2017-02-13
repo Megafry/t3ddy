99a6b088712957f6f594ba92481708f8d7e87bd7
Armin Vieweg <armin@v.ieweg.de>
2017-02-13 23:38:21 +0100

    [TASK] Fix psr-2 style of TabOrder class

--- Classes/Ajax/TabOrder.php
+++ Classes/Ajax/TabOrder.php
@@ -15,104 +15,109 @@ use \TYPO3\CMS\Core\Utility\GeneralUtility;
  *
  * @package ArminVieweg\T3ddy
  */
-class TabOrder {
+class TabOrder
+{
 
-	/**
-	 * Perform change of sortings of given tab.
-	 * Outputs json string response.
-	 *
-	 * @return void
-	 */
-	public function change() {
-		$databaseConnection = DatabaseUtility::getDatabaseConnection();
-		$parameters = GeneralUtility::_GPmerged('t3ddy');
-		$tabUid = intval($parameters['tabUid']);
-		$difference = intval($parameters['difference']);
+    /**
+     * Perform change of sortings of given tab.
+     * Outputs json string response.
+     *
+     * @return void
+     */
+    public function change()
+    {
+        $databaseConnection = DatabaseUtility::getDatabaseConnection();
+        $parameters = GeneralUtility::_GPmerged('t3ddy');
+        $tabUid = intval($parameters['tabUid']);
+        $difference = intval($parameters['difference']);
 
-		if ($difference === 0) {
-			return;
-		}
+        if ($difference === 0) {
+            return;
+        }
 
-			// Tab to change sorting of
-		$thisTabRow = $databaseConnection->exec_SELECTgetSingleRow(
-			'sorting,tx_gridelements_container', 'tt_content', 'uid=' . $tabUid
-		);
+        // Tab to change sorting of
+        $thisTabRow = $databaseConnection->exec_SELECTgetSingleRow(
+            'sorting,tx_gridelements_container',
+            'tt_content',
+            'uid=' . $tabUid
+        );
 
-			// All tabs in this grid container
-		$tabs = $databaseConnection->exec_SELECTgetRows(
-			'uid,sorting',
-			'tt_content',
-			'tx_gridelements_container=' . intval($thisTabRow['tx_gridelements_container']) . ' AND deleted=0',
-			'',
-			'sorting asc'
-		);
+        // All tabs in this grid container
+        $tabs = $databaseConnection->exec_SELECTgetRows(
+            'uid,sorting',
+            'tt_content',
+            'tx_gridelements_container=' . intval($thisTabRow['tx_gridelements_container']) . ' AND deleted=0',
+            '',
+            'sorting asc'
+        );
 
-			// Index of this tab in grid container
-		$index = 0;
-		foreach ($tabs as $key => $tab) {
-			if (intval($tab['uid']) === $tabUid) {
-				$index = $key;
-				break;
-			}
-		}
+        // Index of this tab in grid container
+        $index = 0;
+        foreach ($tabs as $key => $tab) {
+            if (intval($tab['uid']) === $tabUid) {
+                $index = $key;
+                break;
+            }
+        }
 
-		if ($difference < 0) {
-				// Shifting up
-			for ($i = $index; $i > $index + $difference; $i--) {
-				$thisTab = $tabs[$i];
-				$tabs[$i] = $tabs[$i - 1];
-				$temporarySorting = $tabs[$i]['sorting'];
-				$tabs[$i]['sorting'] = $thisTab['sorting'];
-				$thisTab['sorting'] = $temporarySorting;
-				$tabs[$i - 1] = $thisTab;
-			}
-		} else {
-				// Shifting down
-			for ($i = $index; $i < $index + $difference; $i++) {
-				$thisTab = $tabs[$i];
-				$tabs[$i] = $tabs[$i + 1];
-				$temporarySorting = $tabs[$i]['sorting'];
-				$tabs[$i]['sorting'] = $thisTab['sorting'];
-				$thisTab['sorting'] = $temporarySorting;
-				$tabs[$i + 1] = $thisTab;
-			}
-		}
+        if ($difference < 0) {
+            // Shifting up
+            for ($i = $index; $i > $index + $difference; $i--) {
+                $thisTab = $tabs[$i];
+                $tabs[$i] = $tabs[$i - 1];
+                $temporarySorting = $tabs[$i]['sorting'];
+                $tabs[$i]['sorting'] = $thisTab['sorting'];
+                $thisTab['sorting'] = $temporarySorting;
+                $tabs[$i - 1] = $thisTab;
+            }
+        } else {
+            // Shifting down
+            for ($i = $index; $i < $index + $difference; $i++) {
+                $thisTab = $tabs[$i];
+                $tabs[$i] = $tabs[$i + 1];
+                $temporarySorting = $tabs[$i]['sorting'];
+                $tabs[$i]['sorting'] = $thisTab['sorting'];
+                $thisTab['sorting'] = $temporarySorting;
+                $tabs[$i + 1] = $thisTab;
+            }
+        }
 
-		$errors = $this->performTabsUpdate($tabs);
-		if (!empty($errors)) {
-			echo json_encode(array(
-				'status' => 'error',
-				'uids' => $errors
-			));
-			return;
-		}
+        $errors = $this->performTabsUpdate($tabs);
+        if (!empty($errors)) {
+            echo json_encode(array(
+                'status' => 'error',
+                'uids' => $errors
+            ));
+            return;
+        }
 
-		echo json_encode(array(
-			'status' => 'ok'
-		));
-	}
+        echo json_encode(array(
+            'status' => 'ok'
+        ));
+    }
 
-	/**
-	 * Performs database update of new tab sortings
-	 *
-	 * @param array $tabs
-	 * @return array with uids which failed during update. Empty if everything is ok.
-	 */
-	protected function performTabsUpdate(array $tabs) {
-		$errors = array();
-		foreach ($tabs as $tab) {
-			$status = DatabaseUtility::getDatabaseConnection()->exec_UPDATEquery(
-				'tt_content',
-				'uid=' . intval($tab['uid']),
-				array(
-					'sorting' => intval($tab['sorting'])
-				)
-			);
-			if (!$status) {
-				$errors[] = $tab['uid'];
-			}
-		}
-		return $errors;
-	}
+    /**
+     * Performs database update of new tab sortings
+     *
+     * @param array $tabs
+     * @return array with uids which failed during update. Empty if everything is ok.
+     */
+    protected function performTabsUpdate(array $tabs)
+    {
+        $errors = array();
+        foreach ($tabs as $tab) {
+            $status = DatabaseUtility::getDatabaseConnection()->exec_UPDATEquery(
+                'tt_content',
+                'uid=' . intval($tab['uid']),
+                array(
+                    'sorting' => intval($tab['sorting'])
+                )
+            );
+            if (!$status) {
+                $errors[] = $tab['uid'];
+            }
+        }
+        return $errors;
+    }
 
 }
