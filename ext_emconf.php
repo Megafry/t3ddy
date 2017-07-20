ae88a03601a4d4cc871f2cf6f30e43949fb78372
Armin Vieweg <armin@v.ieweg.de>
2017-07-20 16:06:12 +0200

    [TASK] Update ext_emconf.php and raise version to 1.2.0-dev

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,35 +1,28 @@
 <?php
 
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = [
 	'title' => 't3ddy - Super cute tabs and accordions',
 	'description' => 't3ddy provides comfortable tabs and accordion handling right in TYPO3 page module, using the techniques of gridelements.',
-	'category' => 'Backend',
+	'category' => 'be',
 	'author' => 'Armin Ruediger Vieweg',
 	'author_email' => 'armin@v.ieweg.de',
 	'author_company' => '',
-	'shy' => '',
-	'dependencies' => '',
-	'conflicts' => '',
-	'priority' => '',
-	'module' => '',
 	'state' => 'stable',
-	'internal' => '',
 	'uploadfolder' => 0,
 	'createDirs' => '',
-	'modify_tables' => '',
 	'clearCacheOnLoad' => 0,
-	'lockType' => '',
-	'version' => '1.1.2',
-	'constraints' => array(
-		'depends' => array(
-			'typo3' => '7.6.0-7.6.99',
-			'gridelements' => '3.0.5-7.0.99',
-		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-		),
-	),
-	'suggests' => array(
-	),
-);
+	'version' => '1.2.0-dev',
+	'constraints' => [
+		'depends' => [
+			'typo3' => '7.6.0-8.7.99',
+			'gridelements' => '3.0.5-8.0.99',
+        ],
+		'conflicts' => [
+        ],
+		'suggests' => [
+        ],
+    ],
+    'autoload' => [
+        'psr-4' => ['ArminVieweg\\T3ddy\\' => 'Classes']
+    ]
+];
